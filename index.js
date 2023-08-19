061724ee578d6734e5749028f056439b1540d53f
jonathan <j.work.ah@gmail.com>
2023-08-19 21:22:28 +0300

    w

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
 import { exec } from 'child_process';
-exec('ls -a');
+const execute  = (command) => {
+    return exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+        }
+      if (stderr) {
+        console.log(stderr);
+        return;
+      }
+      console.log(stdout);
+    });
+};
+
+execute('ls -a');
 console.log(process.argv[2]);
